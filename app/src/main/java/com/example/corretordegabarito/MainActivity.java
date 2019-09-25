77dc86b53e997522691a13275fa70020097f586f
TheLOL1 <gabriellimati4@gmail.com>
2019-09-24 21:38:05 -0300

    Mostrar fotos em andamento.

--- app/src/main/java/com/example/corretordegabarito/MainActivity.java
+++ app/src/main/java/com/example/corretordegabarito/MainActivity.java
@@ -1,20 +1,23 @@
 package com.example.corretordegabarito;
 
-import android.content.Context;
-import android.content.ContextWrapper;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
-import android.graphics.PixelFormat;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
-import android.provider.SyncStateContract;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -23,16 +26,8 @@ import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-
-    ImageView imageView;
-    ImageView imageView1;
-    ImageView imageView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,19 +35,17 @@ public class MainActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
-        ContextWrapper contextWrapper = new ContextWrapper(getApplicationContext());
-        File diretorio = contextWrapper.getDir("imageDir",Context.MODE_PRIVATE);
-        File file = new File(diretorio,"Branco.png");
+        File file = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES),"Branco.jpg");
         if (file.exists())
         {
             file.delete();
         }
-        file = new File(diretorio,"Oficial.png");
+        file = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES),"Oficial.jpg");
         if (file.exists())
         {
             file.delete();
         }
-        file = new File(diretorio,"Aluno.png");
+        file = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES),"Aluno.jpg");
         if (file.exists())
         {
             file.delete();
@@ -61,205 +54,86 @@ public class MainActivity extends AppCompatActivity {
 
     public void abrirCamera(View view)
     {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (intent.resolveActivity(getPackageManager())!= null)
-        {
-            startActivityForResult(intent,1);
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode,int resultCode,final Intent data)
-    {
-        if (requestCode == 1 && resultCode == RESULT_OK)
-        {
-            AlertDialog.Builder alertdialog = new AlertDialog.Builder(this);
-            alertdialog.setTitle("Tipo do gabarito a ser inserido");
-            final RadioGroup radioGroup = new RadioGroup(this);
-            RadioButton radioButton = new RadioButton(this);
-            RadioButton radioButton1 = new RadioButton(this);
-            RadioButton radioButton2 = new RadioButton(this);
-            radioButton.setText("Branco");
-            radioButton.setTextColor(Color.BLACK);
-            radioButton1.setText("Oficial");
-            radioButton1.setTextColor(Color.BLACK);
-            radioButton2.setText("Aluno");
-            radioButton2.setTextColor(Color.BLACK);
-            radioGroup.addView(radioButton);
-            radioGroup.addView(radioButton1);
-            radioGroup.addView(radioButton2);
-            ((RadioButton)radioGroup.getChildAt(0)).setChecked(true);
-            final LinearLayout linearLayout = new LinearLayout(this);
-            linearLayout.setOrientation(LinearLayout.VERTICAL);
-            linearLayout.addView(radioGroup);
-            alertdialog.setView(linearLayout);
-            alertdialog.setCancelable(false);
-            alertdialog.setNegativeButton("CANCELAR", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
-
-                }
-            });
-            alertdialog.setPositiveButton("INSERIR", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
-                    Bundle bundle = data.getExtras();
-                    Bitmap bitmap = (Bitmap) bundle.get("data");
-                    ContextWrapper contextWrapper = new ContextWrapper(getApplicationContext());
-                    File diretorio = contextWrapper.getDir("imageDir", Context.MODE_PRIVATE);
-                    FileOutputStream fileOutputStream = null;
-                    RadioButton radioButton3 = linearLayout.findViewById(radioGroup.getCheckedRadioButtonId());
-                    String s = radioButton3.getText().toString();
-                    if (s.equals("Branco"))
-                    {
-                        try {
-                            File caminho = new File(diretorio,"Branco.png");
-                            fileOutputStream = new FileOutputStream(caminho);
-                            bitmap.compress(Bitmap.CompressFormat.PNG,100,fileOutputStream);
-                            Toast.makeText(MainActivity.this,"Gabarito do tipo branco inserido com sucesso!",Toast.LENGTH_LONG).show();
-                        }
-                        catch (Exception e)
-                        {
-                            e.printStackTrace();
-                        }
-                        finally {
-                            try {
-                                fileOutputStream.close();
-                            }
-                            catch (IOException e)
-                            {
-                                e.printStackTrace();
-                            }
-                        }
-                    }
-                    else if(s.equals("Oficial"))
-                    {
-                        try {
-                            File caminho = new File(diretorio,"Oficial.png");
-                            fileOutputStream = new FileOutputStream(caminho);
-                            bitmap.compress(Bitmap.CompressFormat.PNG,100,fileOutputStream);
-                            Toast.makeText(MainActivity.this,"Gabarito do tipo oficial inserido com sucesso!",Toast.LENGTH_LONG).show();
-                        }
-                        catch (Exception e)
-                        {
-                            e.printStackTrace();
-                        }
-                        finally {
-                            try {
-                                fileOutputStream.close();
-                            }
-                            catch (IOException e)
-                            {
-                                e.printStackTrace();
-                            }
-                        }
-                    }
-                    else if(s.equals("Aluno"))
-                    {
-                        try {
-                            File caminho = new File(diretorio,"Aluno.png");
-                            fileOutputStream = new FileOutputStream(caminho);
-                            bitmap.compress(Bitmap.CompressFormat.PNG,100,fileOutputStream);
-                            Toast.makeText(MainActivity.this,"Gabarito do tipo aluno inserido com sucesso!",Toast.LENGTH_LONG).show();
-                        }
-                        catch (Exception e)
-                        {
-                            e.printStackTrace();
-                        }
-                        finally {
-                            try {
-                                fileOutputStream.close();
-                            }
-                            catch (IOException e)
-                            {
-                                e.printStackTrace();
-                            }
-                        }
-                    }
-                }
-            });
-            alertdialog.show();
-        }
-    }
-
-    public void CarregarImagem()
-    {
-        ContextWrapper contextWrapper = new ContextWrapper(getApplicationContext());
-        File diretorio = contextWrapper.getDir("imageDir",Context.MODE_PRIVATE);
-        try
-        {
-            File file = new File(diretorio,"Branco.png");
-            Bitmap bitmap = BitmapFactory.decodeStream(new FileInputStream(file));
-            imageView = new ImageView(this);
-            imageView.setImageBitmap(bitmap);
-        }
-        catch (FileNotFoundException e)
-        {
-            imageView = null;
-            e.printStackTrace();
-        }
-        try
-        {
-            File file = new File(diretorio,"Oficial.png");
-            Bitmap bitmap = BitmapFactory.decodeStream(new FileInputStream(file));
-            imageView1 = new ImageView(this);
-            imageView1.setImageBitmap(bitmap);
-        }
-        catch (FileNotFoundException e)
-        {
-            imageView1 = null;
-            e.printStackTrace();
-        }
-        try
-        {
-            File file = new File(diretorio,"Aluno.png");
-            Bitmap bitmap = BitmapFactory.decodeStream(new FileInputStream(file));
-            imageView2 = new ImageView(this);
-            imageView2.setImageBitmap(bitmap);
-        }
-        catch (FileNotFoundException e)
-        {
-            imageView2 = null;
-            e.printStackTrace();
-        }
-    }
-
-    public void MostrarFotos(View view)
-    {
-        final AlertDialog.Builder alertdialog = new AlertDialog.Builder(this);
-        CarregarImagem();
-        if (imageView != null) {
-            imageView.getLayoutParams().height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PT, 300, getResources().getDisplayMetrics());
-            imageView.getLayoutParams().width = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PT, 300, getResources().getDisplayMetrics());
-        }
-        if (imageView1 != null) {
-            imageView1.getLayoutParams().height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PT, 300, getResources().getDisplayMetrics());
-            imageView1.getLayoutParams().width = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PT, 300, getResources().getDisplayMetrics());
-        }
-        if (imageView2 != null) {
-            imageView2.getLayoutParams().height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PT, 300, getResources().getDisplayMetrics());
-            imageView2.getLayoutParams().width = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PT, 300, getResources().getDisplayMetrics());
-        }
-        LinearLayout linearLayout = new LinearLayout(this);
+        AlertDialog.Builder alertdialog = new AlertDialog.Builder(this);
+        alertdialog.setTitle("Tipo do gabarito a ser inserido");
+        final RadioGroup radioGroup = new RadioGroup(this);
+        RadioButton radioButton = new RadioButton(this);
+        RadioButton radioButton1 = new RadioButton(this);
+        RadioButton radioButton2 = new RadioButton(this);
+        radioButton.setText("Branco");
+        radioButton.setTextColor(Color.BLACK);
+        radioButton1.setText("Oficial");
+        radioButton1.setTextColor(Color.BLACK);
+        radioButton2.setText("Aluno");
+        radioButton2.setTextColor(Color.BLACK);
+        radioGroup.addView(radioButton);
+        radioGroup.addView(radioButton1);
+        radioGroup.addView(radioButton2);
+        ((RadioButton)radioGroup.getChildAt(0)).setChecked(true);
+        final LinearLayout linearLayout = new LinearLayout(this);
         linearLayout.setOrientation(LinearLayout.VERTICAL);
-        if (imageView != null) {
-            linearLayout.addView(imageView);
-        }
-        if (imageView1 != null) {
-            linearLayout.addView(imageView1);
-        }
-        if (imageView2 != null) {
-            linearLayout.addView(imageView2);
-        }
+        linearLayout.addView(radioGroup);
         alertdialog.setView(linearLayout);
-        alertdialog.setNeutralButton("FECHAR", new DialogInterface.OnClickListener() {
+        alertdialog.setCancelable(false);
+        alertdialog.setNegativeButton("CANCELAR", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialog, int which) {
+            public void onClick(DialogInterface dialogInterface, int i) {
 
             }
         });
-        if (imageView != null || imageView1 != null || imageView2 != null) {
-            alertdialog.show();
+        alertdialog.setPositiveButton("PROSSEGUIR", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                RadioButton radioButton3 = linearLayout.findViewById(radioGroup.getCheckedRadioButtonId());
+                String s = radioButton3.getText().toString();
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (s.equals("Branco"))
+                {
+                    if (intent.resolveActivity(getPackageManager())!= null)
+                    {
+                        File foto = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES),"Branco.jpg");
+                        Uri uri = FileProvider.getUriForFile(getApplicationContext(),"com.example.corretordegabarito",foto);
+                        intent.putExtra(MediaStore.EXTRA_OUTPUT,uri);
+                        startActivityForResult(intent, 1);
+                        Toast.makeText(getApplicationContext(),"Gabarito do tipo branco inserido com sucesso!",Toast.LENGTH_LONG).show();
+                    }
+                }
+                else if(s.equals("Oficial"))
+                {
+                    if (intent.resolveActivity(getPackageManager())!= null)
+                    {
+                        File foto = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES),"Oficial.jpg");
+                        Uri uri = FileProvider.getUriForFile(getApplicationContext(),"com.example.corretordegabarito",foto);
+                        intent.putExtra(MediaStore.EXTRA_OUTPUT,uri);
+                        startActivityForResult(intent, 1);
+                        Toast.makeText(getApplicationContext(),"Gabarito do tipo oficial inserido com sucesso!",Toast.LENGTH_LONG).show();
+                    }
+                }
+                else if(s.equals("Aluno"))
+                {
+                    if (intent.resolveActivity(getPackageManager())!= null)
+                    {
+                        File foto = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES),"Aluno.jpg");
+                        Uri uri = FileProvider.getUriForFile(getApplicationContext(),"com.example.corretordegabarito",foto);
+                        intent.putExtra(MediaStore.EXTRA_OUTPUT,uri);
+                        startActivityForResult(intent, 1);
+                        Toast.makeText(getApplicationContext(),"Gabarito do tipo aluno inserido com sucesso!",Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+        alertdialog.show();
+    }
+
+    public void MostrarFotos(View view) throws Exception
+    {
+        Intent intent = new Intent(this,mostrarfotos.class);
+        File file = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES),"Branco.jpg");
+        File file1 = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES),"Oficial.jpg");
+        File file2 = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES),"Aluno.jpg");
+        if (file.exists() || file1.exists() || file2.exists())
+        {
+            startActivity(intent);
         }
     }
 }
---
+++ app/src/main/java/com/example/corretordegabarito/mostrarfotos.java
@@ -0,0 +1,127 @@
+package com.example.corretordegabarito;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.File;
+
+public class mostrarfotos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_mostrarfotos);
+        ImageView imageView = findViewById(R.id.imageView);
+        ImageView imageView1 = findViewById(R.id.imageView2);
+        ImageView imageView2 = findViewById(R.id.imageView3);
+        File file = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES),"Branco.jpg");
+        if (file.exists())
+        {
+            ExifInterface exifInterface = null;
+            try {
+                exifInterface = new ExifInterface(file.getAbsolutePath());
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+            int orientacao = exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION,1);
+            Matrix matrix = new Matrix();
+            if (orientacao == ExifInterface.ORIENTATION_ROTATE_90) {
+                matrix.postRotate(90);
+            }
+            else if(orientacao == ExifInterface.ORIENTATION_ROTATE_180)
+            {
+                matrix.postRotate(180);
+            }
+            else if(orientacao == ExifInterface.ORIENTATION_ROTATE_270)
+            {
+                matrix.postRotate(270);
+            }
+            Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
+            Bitmap bitmap1 = Bitmap.createBitmap(bitmap,0,0,bitmap.getWidth(),bitmap.getHeight(),matrix,true);
+            imageView.setImageBitmap(bitmap1);
+        }
+        else
+        {
+            TextView textView = findViewById(R.id.textView4);
+            textView.setText("");
+        }
+        File file1 = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES),"Oficial.jpg");
+        if (file1.exists())
+        {
+            ExifInterface exifInterface = null;
+            try {
+                exifInterface = new ExifInterface(file1.getAbsolutePath());
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+            int orientacao = exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION,1);
+            Matrix matrix = new Matrix();
+            if (orientacao == ExifInterface.ORIENTATION_ROTATE_90) {
+                matrix.postRotate(90);
+            }
+            else if(orientacao == ExifInterface.ORIENTATION_ROTATE_180)
+            {
+                matrix.postRotate(180);
+            }
+            else if(orientacao == ExifInterface.ORIENTATION_ROTATE_270)
+            {
+                matrix.postRotate(270);
+            }
+            Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
+            Bitmap bitmap1 = Bitmap.createBitmap(bitmap,0,0,bitmap.getWidth(),bitmap.getHeight(),matrix,true);
+            imageView1.setImageBitmap(bitmap1);
+        }
+        else
+        {
+            TextView textView = findViewById(R.id.textView5);
+            textView.setText("");
+        }
+        File file2 = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES),"Aluno.jpg");
+        if (file2.exists())
+        {
+            ExifInterface exifInterface = null;
+            try {
+                exifInterface = new ExifInterface(file2.getAbsolutePath());
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+            int orientacao = exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION,1);
+            Matrix matrix = new Matrix();
+            if (orientacao == ExifInterface.ORIENTATION_ROTATE_90) {
+                matrix.postRotate(90);
+            }
+            else if(orientacao == ExifInterface.ORIENTATION_ROTATE_180)
+            {
+                matrix.postRotate(180);
+            }
+            else if(orientacao == ExifInterface.ORIENTATION_ROTATE_270)
+            {
+                matrix.postRotate(270);
+            }
+            Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
+            Bitmap bitmap1 = Bitmap.createBitmap(bitmap,0,0,bitmap.getWidth(),bitmap.getHeight(),matrix,true);
+            imageView2.setImageBitmap(bitmap1);
+        }
+        else
+        {
+            TextView textView = findViewById(R.id.textView6);
+            textView.setText("");
+        }
+    }
+}
