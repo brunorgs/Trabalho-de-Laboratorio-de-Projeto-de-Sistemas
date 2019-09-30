2e2f320f97c9d60a256f333e11a3a578dd4a0a98
TheLOL1 <gabriellimati4@gmail.com>
2019-09-29 21:47:50 -0300

    Botões cinza e limiarizada adicionados.

--- app/src/main/java/com/example/corretordegabarito/mostrarfotos.java
+++ app/src/main/java/com/example/corretordegabarito/mostrarfotos.java
@@ -1,5 +1,6 @@
 package com.example.corretordegabarito;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
@@ -23,7 +24,9 @@ public class mostrarfotos extends AppCompatActivity {
         File file1 = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES),"Branco.jpg");
         File file2 = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES),"Oficial.jpg");
         File file3 = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES),"Aluno.jpg");
-        AdapterMostrarFotos adapterMostrarFotos = new AdapterMostrarFotos(file1,file2,file3);
+        Intent intent = getIntent();
+        char tipo = intent.getCharExtra("tipo",' ');
+        AdapterMostrarFotos adapterMostrarFotos = new AdapterMostrarFotos(file1,file2,file3,tipo);
         recyclerView.setAdapter(adapterMostrarFotos);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setHasFixedSize(true);
